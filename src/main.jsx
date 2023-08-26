4e5ab00c52e6dbbf0fdee8af5cbc3a5eedbc5a6a
Kwame007 <clementnyankuadjei@gmail.com>
2023-08-26 15:24:59 +0000

    import react-dom from react-dom/client

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,7 @@
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom/client.js";
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
